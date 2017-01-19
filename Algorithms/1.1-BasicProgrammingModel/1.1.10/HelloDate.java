222cd4dfb9b35b076c9b0e67ba35ab35f35b7f43
ZhenyuLeng <lengzhenyu@ict.ac.cn>
2017-01-19 23:04:23 +0800

    1.1.12

---
+++ Algorithms/1.1-BasicProgrammingModel/1.1.10/HelloDate.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class HelloDate {
+  public static void main(String[] args) {
+    int[] a = new int[10];
+    for (int i = 0; i < 10; i++)
+      a[i] = i * i;
+  }
+}
