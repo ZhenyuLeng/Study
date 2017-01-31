40cbf2a7e53a23cc083d72f799e87282afee48ae
ZhenyuLeng <lengzhenyu@ict.ac.cn>
2017-02-01 00:19:53 +0800

    1.1.13

--- Algorithms/1.1-BasicProgrammingModel/1.1.13/Transposition.java
+++ Algorithms/1.1-BasicProgrammingModel/1.1.13/Transposition.java
@@ -7,16 +7,40 @@ public class Transposition {
     
     random = new Random(seed);
 
-    int N = 5;
-    int[][] matric = new int[N][N];
+    int M = 5;
+    int N = 3;
+    int matric[][] = new int[M][N];
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < M; i++) {
       for (int j = 0; j < N; j++) {
         matric[i][j] = StdRandom.uniform(100);
-//        matric[i][j] = random.nextInt(100);
         System.out.printf("%d\t", matric[i][j]);
       }
       System.out.println("");
     }
+
+    System.out.println("");
+
+    int b[][] = transposition(matric);
+
+    for (int i = 0; i < b.length; i++) {
+      for (int j = 0; j < b[i].length; j++) {
+        System.out.printf("%d\t", b[i][j]);
+      }
+      System.out.println("");
+    }
   }
+
+  public static int[][] transposition(int a[][]) {
+    int b[][] = new int[a[0].length][a.length];
+
+    for (int i = 0; i < b.length; i++) {
+      for (int j = 0; j < b[i].length; j++) {
+        b[i][j] = a[a.length - j - 1][a[i].length - i - 1];
+      }
+    }
+ 
+    return b;
+  }
+
 }
