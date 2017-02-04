5ead34c4925f2552f2427397e30df182705c965e
ZhenyuLeng <lengzhenyu@ict.ac.cn>
2017-02-04 23:40:14 +0800

    1.1.22 original BinarySearch

---
+++ Algorithms/1.1-BasicProgrammingModel/1.1.22/BinarySearch.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class BinarySearch
+{
+  public static int rank(int key, int[] a)
+  {
+    return rank(key, a, 0, a.length - 1);
+  }
+
+  public static int rank(int key, int[] a, int lo, int hi)
+  {
+    if (lo > hi) return -1;
+    int mid = lo + (hi - lo) / 2;
+    if      (key < a[mid]) return rank(key, a, lo, mid - 1);
+    else if (key > a[mid]) return rank(key, a, mid + 1, hi);
+    else                   return mid;
+  }
+
+  public static void main(String[] args)
+  {
+    int[] whitelist = In.readInts(args[0]);
+
+    Arrays.sort(whitelist);
+
+    while (!StdIn.isEmpty())
+    { // Read key, print if not in whitelist.
+      int key = StdIn.readInt();
+      if (rank(key, whitelist) < 0)
+        StdOut.println(key);
+    }
+  }
+}
