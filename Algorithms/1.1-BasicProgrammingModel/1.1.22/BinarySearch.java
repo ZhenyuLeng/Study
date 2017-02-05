27338847b6cd907150cacaacbd15cfc4cddf097d
ZhenyuLeng <lengzhenyu@ict.ac.cn>
2017-02-05 16:17:52 +0800

    1.1.22v2

--- Algorithms/1.1-BasicProgrammingModel/1.1.22/BinarySearch.java
+++ Algorithms/1.1-BasicProgrammingModel/1.1.22/BinarySearch.java
@@ -4,15 +4,16 @@ public class BinarySearch
 {
   public static int rank(int key, int[] a)
   {
-    return rank(key, a, 0, a.length - 1);
+    return rank(key, a, 0, a.length - 1, 0);
   }
 
-  public static int rank(int key, int[] a, int lo, int hi)
+  public static int rank(int key, int[] a, int lo, int hi, int depth)
   {
+    StdOut.println(lo + " " + depth + " " + hi);
     if (lo > hi) return -1;
     int mid = lo + (hi - lo) / 2;
-    if      (key < a[mid]) return rank(key, a, lo, mid - 1);
-    else if (key > a[mid]) return rank(key, a, mid + 1, hi);
+    if      (key < a[mid]) return rank(key, a, lo, mid - 1, depth + 1);
+    else if (key > a[mid]) return rank(key, a, mid + 1, hi, depth + 1);
     else                   return mid;
   }
 
