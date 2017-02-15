f05493a371d5d210f3db1b02c117a9496e59c48f
ZhenyuLeng <lengzhenyu@ict.ac.cn>
2017-02-15 11:11:51 +0800

    C++4.2

---
+++ C++PrimerPlus-SixthEdition/c4/4.2/42.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    using namespace std;
+    string first_name;
+    string last_name;
+    char grade;
+    int age;
+  
+    cout << "What is your first name? ";
+    getline(cin, first_name);
+  
+    cout << "What is your last name? ";
+    getline(cin, last_name);
+
+    cout << "What letter grade do you deserve? ";
+    cin >> grade;
+    grade = (char) (grade + 1);
+
+    cout << "What is your age? ";
+    (cin >> age).get();
+
+    cout << "Name: " << last_name << ", " << first_name << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Age: " << age << endl;
+
+    return 0;
+}
